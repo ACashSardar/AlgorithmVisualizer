a581ca5ed5cf6d7c1856da59f20dc460f6da3a30
Akash <akashsardar383@gmail.com>
2022-11-26 20:15:43 +0530

    Commit1

--- main.js
+++ main.js
@@ -2,17 +2,38 @@ const logs = document.getElementById("logs");
 const audio = document.getElementById("audio");
 const barList = document.getElementById("bar-list");
 const complexity = document.getElementById("complexity");
+const runtime = document.getElementById("runtime");
+const arraySize = document.getElementById("array-size");
+
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 let verticalBars = [];
 let elements = [];
 let sortingAlgo = "bubbleSort";
 let animationTime = 1;
+let sampleCount = 50;
 let isSorted = false;
+let running = false;
+
+arraySize.innerHTML = `n: ${sampleCount}`;
 
 function adjustAnimation(e) {
   animationTime = parseInt(e.target.value) * 50;
 }
+
+function adjustBarWidth(e) {
+  if (running) return;
+  sampleCount = parseInt(e.target.value);
+  arraySize.innerHTML = `n: ${sampleCount}`;
+
+  Array.from(verticalBars).forEach((bar, index) => {
+    bar.style.borderWidth =
+      (100 / sampleCount) * window.innerWidth * 0.00045 + "px";
+  });
+
+  loadRandomSamples();
+}
+
 const pixelToInt = (pixelVal) => {
   rawStr = pixelVal.substring(0, pixelVal.length - 2);
   return parseInt(rawStr);
@@ -32,9 +53,15 @@ const swap = (arr, i, j) => {
 
 const setUpBarHeights = (bars, elements) => {
   const max = elements.reduce((a, b) => Math.max(a, b), -Infinity);
+
+  const dx = 99 / elements.length;
+  console.log(elements.length, dx);
+
   Array.from(bars).forEach((bar, index) => {
     bar.style.height = (elements[index] * 200) / max + "px";
-    bar.style.left = 5 + index * 10 + "px";
+    bar.style.left = 1 + index * dx + "%";
+    bar.style.borderWidth =
+      (100 / sampleCount) * window.innerWidth * 0.0045 + "px";
   });
 };
 
@@ -46,7 +73,7 @@ const merge = async (bars, low, mid, high) => {
 
   logs.innerHTML += `<p>low: ${low},mid: ${mid}, high: ${high}</p>`;
 
-  bars[mid].style.borderLeftColor = "limegreen";
+  bars[mid].style.borderLeftColor = "hotpink";
 
   for (let i = 0; i < n; i++) left[i] = pixelToInt(bars[low + i].style.height);
   for (let j = 0; j < m; j++)
@@ -59,7 +86,7 @@ const merge = async (bars, low, mid, high) => {
 
   audio.play();
 
-  logs.innerHTML += `<p class="text-warning">Merging: ${left} with ${right}</p>`;
+  logs.innerHTML += `<p class="text-light">Merging: ${left} with ${right}</p>`;
 
   while (i < n && j < m) {
     if (left[i] < right[j]) bars[k++].style.height = left[i++] + "px";
@@ -69,21 +96,22 @@ const merge = async (bars, low, mid, high) => {
   while (i < n) bars[k++].style.height = left[i++] + "px";
   while (j < m) bars[k++].style.height = right[j++] + "px";
 
-  bars[high].style.borderLeftColor = "limegreen";
+  bars[high].style.borderLeftColor = "hotpink";
 };
 
 const bubbleSort = async (bars) => {
   logs.innerHTML += "<p>Starting Bubble Sort...</p>";
   for (let i = 0; i < bars.length; i++) {
     for (let j = 0; j < bars.length - i - 1; j++) {
-      bars[j].style.borderLeftColor = "black";
+      bars[j].style.borderLeftColor = "white";
       if (
         pixelToInt(bars[j].style.height) > pixelToInt(bars[j + 1].style.height)
       ) {
-        logs.innerHTML += `<p class="text-warning" >Swapping ${pixelToInt(
+        logs.innerHTML += `<p class="text-light" >Swapping ${pixelToInt(
           bars[j].style.height
         )} with ${pixelToInt(bars[j + 1].style.height)}</p>`;
         swapBars(bars, j, j + 1);
+
         bars[j + 1].style.borderLeftColor = "lime";
         await timer(animationTime);
       }
@@ -115,14 +143,14 @@ const quickSort = async (bars, low, high) => {
   }
   audio.play();
   swapBars(bars, low, j);
-  logs.innerHTML += `<p class="text-warning">Swapping Pivot with location ${j}</p>`;
-  bars[low].style.borderLeftColor = "lightseagreen";
-  bars[j].style.borderLeftColor = "lightseagreen";
+  logs.innerHTML += `<p class="text-light">Swapping Pivot with location ${j}</p>`;
+  bars[low].style.borderLeftColor = "greenyellow";
+  bars[j].style.borderLeftColor = "greenyellow";
 
   await quickSort(bars, low, j - 1);
 
   await quickSort(bars, j + 1, high);
-  bars[high].style.borderLeftColor = "lightseagreen";
+  bars[high].style.borderLeftColor = "greenyellow";
 };
 
 const mergeSort = async (bars, low, high) => {
@@ -144,8 +172,9 @@ const selectionSort = async (bars) => {
       ) {
         minValIndx = j;
       } else {
-        bars[j].style.borderLeftColor = "black";
+        bars[j].style.borderLeftColor = "white";
       }
+      await timer(animationTime);
     }
 
     bars[minValIndx].style.borderLeftColor = "orangered";
@@ -154,22 +183,23 @@ const selectionSort = async (bars) => {
       bars[minValIndx].style.height
     )} found at index ${minValIndx} </p>`;
 
-    logs.innerHTML += `<p class="text-warning">Min Value ${pixelToInt(
+    logs.innerHTML += `<p class="text-light">Min Value ${pixelToInt(
       bars[minValIndx].style.height
     )} swapped with ${pixelToInt(bars[i].style.height)}</p>`;
-    await timer(animationTime);
+
     swapBars(bars, i, minValIndx);
     audio.play();
-    bars[i].style.borderLeftColor = "navy";
+    bars[i].style.borderLeftColor = "lightseagreen";
   }
 };
 
 function loadRandomSamples() {
-  // let length = parseInt(window.innerWidth / 27);
-  let length = 30;
+  let length = sampleCount;
   let max = 100;
 
   barList.innerHTML = "";
+  runtime.innerHTML = "Stopped";
+
   let elements = [...new Array(length)].map(() =>
     Math.round(Math.random() * max)
   );
@@ -191,40 +221,49 @@ function handleSortAlgo(e) {
   sortingAlgo = e.target.value;
 
   if (sortingAlgo === "bubbleSort" || sortingAlgo === "selectionSort") {
-    complexity.innerHTML = "Time Complexity: 0[n^2]";
+    complexity.innerHTML = "TC: 0[n²]";
   } else if (sortingAlgo === "quickSort" || sortingAlgo === "mergeSort") {
-    complexity.innerHTML = "Time Complexity: 0[nlog(n)]";
+    complexity.innerHTML = "TC: 0[nlog(n)]";
   }
 }
 
-function handleSort() {
+async function handleSort() {
   if (isSorted == true) {
     alert("Already sorted!");
     return;
   }
 
+  running = true;
+  runtime.innerHTML = "Started..";
+  let start = new Date().getTime();
   if (sortingAlgo === "bubbleSort") {
     logs.innerHTML = "";
-    bubbleSort(verticalBars);
+    await bubbleSort(verticalBars);
   } else if (sortingAlgo === "quickSort") {
     logs.innerHTML = "<p>Starting Quick Sort..</p>";
-    quickSort(verticalBars, 0, verticalBars.length - 1);
+    await quickSort(verticalBars, 0, verticalBars.length - 1);
   } else if (sortingAlgo === "mergeSort") {
     logs.innerHTML = "<p>Starting Merge Sort..</p>";
-    mergeSort(verticalBars, 0, verticalBars.length - 1);
+    await mergeSort(verticalBars, 0, verticalBars.length - 1);
   } else if (sortingAlgo === "selectionSort") {
     logs.innerHTML = "<p>Starting Selection Sort..</p>";
-    selectionSort(verticalBars);
+    await selectionSort(verticalBars);
   }
+  let stop = new Date().getTime();
   isSorted = true;
+  running = false;
+  if (stop - start < 1000) runtime.innerHTML = "⏱️" + (stop - start) + " ms";
+  else {
+    runtime.innerHTML = "⏱️" + ((stop - start) / 1000).toFixed(2) + " s";
+  }
 }
 
 function clearConsole() {
   logs.innerHTML = "";
 }
 
-// addEventListener("resize", (event) => {
-//   loadRandomSamples();
-// });
+function reset() {
+  location.reload();
+}
 
 loadRandomSamples();
